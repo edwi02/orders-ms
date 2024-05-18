b4ec2388b02d38b97e23f045076dd28e588153d3
Edwin Quintero <edwi02@gmail.com>
2024-05-18 15:24:05 -0500

    feat: section 8 completed

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,20 +4,27 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
-  PRODUCT_MICROSERVICE_HOST: string;
-  PRODUCT_MICROSERVICE_PORT: number;
+  // PRODUCT_MICROSERVICE_HOST: string;
+  // PRODUCT_MICROSERVICE_PORT: number;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     // DATABASE_URL: joi.string().required(),
-    PRODUCT_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCT_MICROSERVICE_PORT: joi.number().required(),
+    // PRODUCT_MICROSERVICE_HOST: joi.string().required(),
+    // PRODUCT_MICROSERVICE_PORT: joi.number().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string().required()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -28,6 +35,8 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   // databaseUrl: envVars.DATABASE_URL,
-  productsMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
-  productsMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT,
+  // productsMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
+  // productsMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT,
+  databaseUrl: envVars.DATABASE_URL,
+  natsServers: envVars.NATS_SERVERS,
 };
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -6,25 +6,25 @@ import { ChangeOrderStatusDto, OrderPaginationDto } from './dto';
 
 @Controller()
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(private readonly client: OrdersService) {}
 
   @MessagePattern('createOrder')
   create(@Payload() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+    return this.client.create(createOrderDto);
   }
 
   @MessagePattern('findAllOrders')
   findAll(@Payload() orderPaginationDto: OrderPaginationDto) {
-    return this.ordersService.findAll(orderPaginationDto);
+    return this.client.findAll(orderPaginationDto);
   }
 
   @MessagePattern('findOneOrder')
   findOne(@Payload('id', ParseUUIDPipe) id: string) {
-    return this.ordersService.findOne(id);
+    return this.client.findOne(id);
   }
 
   @MessagePattern('changeOrderStatus')
   changeOrderStatus(@Payload() changeOrderStatusDto: ChangeOrderStatusDto) {
-    return this.ordersService.changeStatus(changeOrderStatusDto);
+    return this.client.changeStatus(changeOrderStatusDto);
   }
 }
